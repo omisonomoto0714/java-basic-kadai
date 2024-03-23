3f355418d29787faa7f57e4abf2623c2df441253
omisonomoto0714 <159553436+omisonomoto0714@users.noreply.github.com>
2024-03-23 10:58:35 +0900

    kadai_004

---
+++ kadai_004/Score_Chapter04.java
@@ -0,0 +1,20 @@
+package kadai_004;
+
+public class Score_Chapter04 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("Aさん" + 10 + "点");
+		System.out.println("Bさん" + 20 + "点");
+		System.out.println("Cさん" + 30 + "点");
+		System.out.println("Dさん" + 40 + "点");
+		System.out.println("Eさん" + 50 + "点");
+		System.out.println("Fさん" + 60 + "点");
+		System.out.println("Gさん" + 70 + "点");
+		System.out.println("Hさん" + 80 + "点");
+		System.out.println("Iさん" + 90 + "点");
+		System.out.println("Jさん" + 100 + "点");
+		System.out.println((10+ 20 + 30 + 40 + 50 + 60 + 70 + 80 + 90 + 100	)	/ 10);
+	}
+
+}
