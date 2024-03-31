3a74bad4eef92df02ab0d1bb4a596d6a3342324c
omisonomoto0714 <159553436+omisonomoto0714@users.noreply.github.com>
2024-03-31 21:24:54 +0900

    kadai_17

---
+++ kadai_017/KatoIchiro_Chapter17.java
@@ -0,0 +1,12 @@
+package kadai_017;
+
+
+public class KatoIchiro_Chapter17 extends Kato_Chapter17 {
+
+	public void setGivenName() {
+		givenName ="一郎";
+	}
+	 public void eachIntroduce() {
+		 System.out.println("好きな食べ物はリンゴです");
+	}
+}
---
+++ kadai_017/KatoTaro_Chapter17.java
@@ -0,0 +1,14 @@
+package kadai_017;
+
+
+public class KatoTaro_Chapter17 extends Kato_Chapter17 {
+
+			
+	public void setGivenName() {
+		givenName = "太郎";	
+		
+	}
+	 public void eachIntroduce() {
+		 System.out.println("javaが得意です");
+	 }
+}
