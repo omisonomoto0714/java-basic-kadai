e37634f552c0a3d8c77374641b7e6910a3bf253b
omisonomoto0714 <159553436+omisonomoto0714@users.noreply.github.com>
2024-04-08 23:43:47 +0900

    kadai_24

---
+++ kadai024/JyankenExec_Chapter24.java
@@ -0,0 +1,13 @@
+package kadai024;
+
+public class JyankenExec_Chapter24 {
+
+	public static void main(String[] args) {
+
+		Jyanken_Chapter24 jyanken = new Jyanken_Chapter24();
+		
+		
+		jyanken.playGame();
+	}
+
+}
